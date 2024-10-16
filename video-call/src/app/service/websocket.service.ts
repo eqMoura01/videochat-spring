d5641fc4e379b4a591bd4d4034985669b54a70ee
eqMoura01 <victorlvmora@gmail.com>
2024-10-16 15:04:30 -0300

    fix: corrige a implementação até o momento de envio da oferta

--- video-call/src/app/service/websocket.service.ts
+++ video-call/src/app/service/websocket.service.ts
@@ -60,8 +60,6 @@ export class WebsocketService {
         // Lógica para enviar oferta
         break;
       case 'offer':
-
-        console.log('Offer received ASIDHuahsudhasuh');
         // Lógica para receber oferta
         break;
       case 'answer':
